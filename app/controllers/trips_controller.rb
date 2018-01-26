cf9b8176c94fdae644614ef08c5f2c892f34c573
rcoras <rcoras@yahoo.com>
2018-01-26 13:53:51 -0500

    Added user_id to trips params in trips controller
    Also added user_id as parameter to curl script to test successfully

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -46,6 +46,6 @@ class TripsController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def trip_params
-      params.require(:trip).permit(:trip_date, :trip_length_hrs, :location, :no_of_fish_caught, :comments)
+      params.require(:trip).permit(:trip_date, :trip_length_hrs, :location, :no_of_fish_caught, :comments, :user_id)
     end
 end
